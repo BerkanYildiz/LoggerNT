3eb6598af07a1864fd993e22a7fc4c4c2e6d1068
BerkanYildiz <BerkanYildiz@users.noreply.github.com>
2021-07-21 18:43:19 +0200

    :ant:

--- src/Headers/LoggerNT.h
+++ src/Headers/LoggerNT.h
@@ -1,23 +1,14 @@
 #pragma once
 
 // 
-// One of the new "developers" (they had to fill the quota for their diversity program) had sex with
-// the compiler + some of the main windows headers files and gave it STDs so now you cannot compile without this define. :))
-// 
-
-#define _NO_CRT_STDIO_INLINE
-
-// 
 // Include the basic windows kernel headers.
 // 
 
 #include <ntdef.h>
 #include <ntifs.h>
 #include <vadefs.h>
-#include <tchar.h>
 #include <wdm.h>
 #include <ntstrsafe.h>
-#include <cstdarg>
 
 // 
 // Define the library globals.
