a64ee154609235285aecc736499a9cfec331033b
BerkanYildiz <BerkanYildiz@users.noreply.github.com>
2025-07-26 22:06:38 +0200

    Update SerialPortProvider.hpp

--- src/Headers/Providers/SerialPortProvider.hpp
+++ src/Headers/Providers/SerialPortProvider.hpp
@@ -18,31 +18,31 @@ public:
 		switch (InLogLevel)
 		{
 			case ELogLevel::Trace:
-				SerialWrite(L" TRACE : ", 9);
+				SerialWrite(L"   TRACE   :  ", 14);
 				break;
 			
 			case ELogLevel::Debug:
-				SerialWrite(L" DEBUG : ", 9);
+				SerialWrite(L"   DEBUG   :  ", 14);
 				break;
 			
 			case ELogLevel::Information:
-				SerialWrite(L"  INF  : ", 9);
+				SerialWrite(L"    INF    :  ", 14);
 				break;
 			
 			case ELogLevel::Warning:
-				SerialWrite(L"  WRN  : ", 9);
+				SerialWrite(L"    WRN    :  ", 14);
 				break;
 			
 			case ELogLevel::Error:
-				SerialWrite(L" ERROR : ", 9);
+				SerialWrite(L"   ERROR   :  ", 14);
 				break;
 			
 			case ELogLevel::Fatal:
-				SerialWrite(L" FATAL : ", 9);
+				SerialWrite(L"   FATAL   :  ", 14);
 				break;
 
 			default:
-                SerialWrite(L"  UNK  : ", 9);
+                SerialWrite(L"    UNK    :  ", 14);
 				break;
 		}
 
@@ -65,9 +65,6 @@ private:
 
 	static void SerialWrite(CONST WCHAR* InBuffer, ULONG InLength)
 	{
-		while (!(READ_PORT_UCHAR((PUCHAR) 0x3FD) & 0x20))
-			_mm_pause();
-
-		WRITE_PORT_BUFFER_USHORT((PUSHORT) 0x3F8, (PUSHORT) InBuffer, InLength);
+		WRITE_PORT_BUFFER_UCHAR((PUCHAR) 0x3F8, (PUCHAR) InBuffer, InLength * 2);
     }
 };
